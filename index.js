e88bbfe091b52852916caa87b2a401dbe8b666ae
Aqua Flash <chilko456@gmail.com>
2025-09-20 23:33:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import ImageKit from "imagekit";
 import mongoose from "mongoose";
 import Chat from "./models/chat.js";
 import UserChats from "./models/userChats.js";
-// import FeedBack from "./models/feedBack.js";
 import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node'
 import dotenv from "dotenv";
 dotenv.config();
