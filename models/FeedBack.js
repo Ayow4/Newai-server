ea8bda8651bd65a54153c094c93dd1b074a2f7a3
Aqua Flash <chilko456@gmail.com>
2025-09-20 21:25:29 +0800

    Update FeedBack.js

--- models/FeedBack.js
+++ models/FeedBack.js
@@ -1,25 +1,25 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const feedbackSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: String,
-      required: true,
-    },
-    rating: {
-      type: Number,
-      required: true,
-    },
-    comment: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-    },
-  },
-  { timestamps: true }
-);
+// const feedbackSchema = new mongoose.Schema(
+//   {
+//     userId: {
+//       type: String,
+//       required: true,
+//     },
+//     rating: {
+//       type: Number,
+//       required: true,
+//     },
+//     comment: {
+//       type: String,
+//       required: true,
+//     },
+//     createdAt: {
+//       type: Date,
+//       default: Date.now(),
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-export default mongoose.models.feedBack || mongoose.model("feedback", feedbackSchema);
+// export default mongoose.models.feedBack || mongoose.model("feedback", feedbackSchema);
